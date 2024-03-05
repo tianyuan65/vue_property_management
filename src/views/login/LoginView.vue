91ad63bf1040578bfcc0cb9b30857a089889bbc2
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2024-03-05 20:23:20 +0900

    first commit property_management project

---
+++ src/views/login/LoginView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login">
+    登录页
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'LoginView'
+  }
+</script>
+
+<style lang="scss">
+  .login{
+    background: aqua;
+    height:100%;
+  }
+  html,body,#app{
+    height:100%;
+  }
+</style>

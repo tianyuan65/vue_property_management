682a3b572962c64c37f8e7f7fd08e3fcef6af2f7
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2024-03-06 09:13:32 +0900

    setup default css style

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
+  <h1>我是全局根组件内容</h1>
   <!-- 路由出口 -->
   <router-view/>
 </template>
 
 <style lang="scss">
-
+*{
+  margin: 0;
+  padding:0;
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 设置为true时，在保存文件时会自动执行ESlint检查，在此关闭
+  lintOnSave:false,
+  // 设置css的主入口文件
+  css:{
+    loaderOptions:{
+      scss:{
+        additionalData:`@import "./src/styles/styles.scss";`
+      }
+    }
+  }
 })
